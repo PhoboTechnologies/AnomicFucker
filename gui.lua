e43e0ed2573b57f5c167afcb8395b426635d7efc
phobo <68134116+phobological@users.noreply.github.com>
2022-01-17 00:40:02 -0500

    Gui initial commit
    commit suicide

---
+++ gui.lua
@@ -0,0 +1,83 @@
+local ScreenGui = Instance.new("ScreenGui")
+local main = Instance.new("Frame")
+local LABEL = Instance.new("TextLabel")
+local TextButton = Instance.new("TextButton")
+local TextButton_2 = Instance.new("TextButton")
+local TextButton_3 = Instance.new("TextButton")
+local TextButton_4 = Instance.new("TextButton")
+local TextButton_5 = Instance.new("TextButton")
+local TextButton_6 = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.Workspace
+
+main.Name = "main"
+main.Parent = ScreenGui
+main.BackgroundColor3 = Color3.fromRGB(49, 46, 44)
+main.BorderColor3 = Color3.fromRGB(38, 37, 37)
+main.Position = UDim2.new(0.606469154, 0, 0.592725337, 0)
+main.Size = UDim2.new(0, 501, 0, 314)
+
+LABEL.Name = "LABEL"
+LABEL.Parent = main
+LABEL.BackgroundColor3 = Color3.fromRGB(91, 85, 82)
+LABEL.Size = UDim2.new(0, 501, 0, 50)
+LABEL.Font = Enum.Font.SourceSans
+LABEL.Text = "AF premium b0.1"
+LABEL.TextColor3 = Color3.fromRGB(0, 0, 0)
+LABEL.TextSize = 14.000
+
+TextButton.Parent = main
+TextButton.BackgroundColor3 = Color3.fromRGB(63, 57, 56)
+TextButton.Position = UDim2.new(0.021956088, 0, 0.207006365, 0)
+TextButton.Size = UDim2.new(0, 200, 0, 50)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "Cash dupe"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextSize = 14.000
+
+TextButton_2.Parent = main
+TextButton_2.BackgroundColor3 = Color3.fromRGB(63, 57, 56)
+TextButton_2.Position = UDim2.new(0.580838323, 0, 0.207006365, 0)
+TextButton_2.Size = UDim2.new(0, 200, 0, 50)
+TextButton_2.Font = Enum.Font.SourceSans
+TextButton_2.Text = "Drop guns"
+TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.TextSize = 14.000
+
+TextButton_3.Parent = main
+TextButton_3.BackgroundColor3 = Color3.fromRGB(63, 57, 56)
+TextButton_3.Position = UDim2.new(0.0219560862, 0, 0.464968145, 0)
+TextButton_3.Size = UDim2.new(0, 200, 0, 50)
+TextButton_3.Font = Enum.Font.SourceSans
+TextButton_3.Text = "NoClip"
+TextButton_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_3.TextSize = 14.000
+
+TextButton_4.Parent = main
+TextButton_4.BackgroundColor3 = Color3.fromRGB(63, 57, 56)
+TextButton_4.Position = UDim2.new(0.580838323, 0, 0.464968145, 0)
+TextButton_4.Size = UDim2.new(0, 200, 0, 50)
+TextButton_4.Font = Enum.Font.SourceSans
+TextButton_4.Text = "AimLock"
+TextButton_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_4.TextSize = 14.000
+
+TextButton_5.Parent = main
+TextButton_5.BackgroundColor3 = Color3.fromRGB(63, 57, 56)
+TextButton_5.Position = UDim2.new(0.0219560862, 0, 0.726114631, 0)
+TextButton_5.Size = UDim2.new(0, 200, 0, 50)
+TextButton_5.Font = Enum.Font.SourceSans
+TextButton_5.Text = "Speed +100"
+TextButton_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_5.TextSize = 14.000
+
+TextButton_6.Parent = main
+TextButton_6.BackgroundColor3 = Color3.fromRGB(63, 57, 56)
+TextButton_6.Position = UDim2.new(0.580838323, 0, 0.726114631, 0)
+TextButton_6.Size = UDim2.new(0, 200, 0, 50)
+TextButton_6.Font = Enum.Font.SourceSans
+TextButton_6.Text = "Speed -100"
+TextButton_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_6.TextSize = 14.000
